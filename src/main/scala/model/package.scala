1a0ac2739f1a58085d6f4f8f519b56676cd103f0
Vasyl Koida <vasyl.koida@gmail.com>
2017-11-12 16:10:54 +0200

    added many-to-many tables

--- src/main/scala/model/package.scala
+++ src/main/scala/model/package.scala
@@ -1,3 +1,7 @@
+import java.util.concurrent.TimeUnit
+
+import slick.ast.BaseTypedType
+import slick.jdbc.JdbcType
 import slick.jdbc.PostgresProfile.api._
 
 import scala.concurrent.duration._
@@ -5,5 +9,7 @@ import scala.concurrent.duration._
 package object model {
 
   implicit val durationToLongMapper =
-    MappedColumnType.base[Duration, Long](_.toSeconds, _.seconds)
+    MappedColumnType.base[Duration, Long](
+      (d: Duration) => d.toSeconds,
+      (l: Long) => FiniteDuration(l, TimeUnit.SECONDS))
 }
